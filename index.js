19296507f77eed45c25932b6e6867485bb662c0a
Matthew Curtis <matthew.curtis@owlcyber.com>
2019-02-21 13:56:04 -0600

    Add unit tests and test page

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
  * the list using their preferred method
  * and is inspired by webpackUniversalModuleDefinition
  */
-(function universalModuleDefinition(root, factory){
-  if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory();
-	else if(typeof define === 'function' && define.amd)
-		define("austinTechList", [], factory);
-	else if(typeof exports === 'object')
-		exports["austinTechList"] = factory();
-	else
-		root["austinTechList"] = factory();
+(function universalModuleDefinition(root, factory) {
+  if (typeof exports === 'object' && typeof module === 'object')
+    module.exports = factory();
+  else if (typeof define === 'function' && define.amd)
+    define("austinTechList", [], factory);
+  else if (typeof exports === 'object')
+    exports["austinTechList"] = factory();
+  else
+    root["austinTechList"] = factory();
 })(this, function factory() {
 
   // THE LIST STARTS HERE
@@ -20,10 +20,10 @@
       name: 'Bumble',
       homepage: 'https://bumble.com/',
       careers: 'https://bumble.com/jobs',
-      address: 'West 41st Street, Austin, TX',
+      address: '1105 W 41st St, Austin, TX 78756',
       plusCode: '8755+R3 Austin, Texas',
     },
-    
+
     {
       name: 'Cloudflare',
       homepage: 'https://www.cloudflare.com/',
@@ -36,7 +36,7 @@
       name: 'Keet Health',
       homepage: 'https://www.keethealth.com/',
       careers: 'https://www.keethealth.com/jobs/',
-      address: 'East 6th Street #206, Austin, TX',
+      address: '200 E 6th St #206, Austin, TX 78701',
       plusCode: '7795+4H Austin, Texas',
     },
 
@@ -47,7 +47,7 @@
       address: '3800 N Lamar Blvd #220, Austin, TX 78756',
       plusCode: '8744+FR Austin, Texas',
     },
-    
+
     {
       name: 'uShip',
       homepage: 'https://www.uship.com/',

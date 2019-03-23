ee3063cc073bc3153fcef3a8c7a55f963b2b0bb3
Matthew Curtis <matthew@matthewbryancurtis.com>
2019-03-23 12:26:14 -0500

    Prep for v1 npm release (#52)

--- index.js
+++ index.js
@@ -21,7 +21,7 @@
     {
       name: 'Aunt Bertha',
       homepage: 'https://www.auntbertha.com/',
-      careers: 'https://www.auntbertha.com/',
+      careers: '',
       address: '3429 Executive Center Dr, Austin, TX 78731',
       plusCode: '9753+F3 Austin, Texas'
     },
@@ -32,6 +32,14 @@
       careers: 'https://bloomfire.com/about-us/careers/',
       address: '1717 W 6th St, Austin, TX 78703',
       plusCode: '76GM+FM Austin, Texas'
+    },
+
+    {
+      name: 'Bright Health',
+      homepage: 'https://brighthealthplan.com/',
+      careers: 'https://jobs.lever.co/brighthealthplan',
+      address: '1135 W 6th St Suite 130, Austin, TX 78703',
+      plusCode: '76CR+XX Austin, Texas'
     },
 
     {
@@ -51,6 +59,30 @@
     },
 
     {
+      name: 'CognitiveScale',
+      homepage: 'https://www.cognitivescale.com/',
+      careers: 'https://www.cognitivescale.com/careers/',
+      address: '9500 Arboretum Blvd L-1, Austin, TX 78759',
+      plusCode: '96RW+2J Austin, Texas'
+    },
+
+    {
+      name: 'Drillinginfo',
+      homepage: 'https://info.drillinginfo.com/',
+      careers: 'https://info.drillinginfo.com/careers/',
+      address: '2901 Vía Fortuna #200, Austin, TX 78746',
+      plusCode: '752X+4X Austin, Texas'
+    },
+
+    {
+      name: 'Gartner',
+      homepage: 'https://www.gartner.com/',
+      careers: 'https://jobs.gartner.com/careers',
+      address: '106 East Sixth Street Suite 900, Austin, TX 78701',
+      plusCode: '7795+72 Austin, Texas'
+    },
+
+    {
       name: 'General Motors',
       homepage: 'https://www.gm.com/',
       careers: 'https://search-careers.gm.com/c/information-technology-jobs',
@@ -64,6 +96,22 @@
       careers: 'https://www.homeaway.com/careers/openings/',
       address: '11800 Domain Blvd #300, Austin, TX 78758',
       plusCode: 'C73H+V9 Austin, Texas'
+    },
+
+    {
+      name: 'HotSchedules',
+      homepage: 'https://www.hotschedules.com/',
+      careers: 'https://www.hotschedules.com/careers/',
+      address: '6504 Bridge Point Pkwy Ste 425, Austin, TX 78730',
+      plusCode: '953W+XW Austin, Texas'
+    },
+
+    {
+      name: 'JASK',
+      homepage: 'https://jask.com/',
+      careers: 'https://jask.com/company/careers/',
+      address: '11501 Rock Rose Ave, Suite 200, Austin, TX 78758',
+      plusCode: 'C72G+5V Austin, Texas'
     },
 
     {
@@ -83,11 +131,83 @@
     },
 
     {
+      name: 'Optimizely',
+      homepage: 'https://www.optimizely.com/',
+      careers: 'https://www.optimizely.com/careers/',
+      address: '600 Congress Ave, Austin, TX 78701',
+      plusCode: '7794+8R Austin, Texas'
+    },
+
+    {
+      name: 'Pinpoint',
+      homepage: 'https://pinpoint.com/',
+      careers: 'https://pinpoint.com/careers',
+      address: '11801 Domain Blvd, 3rd Floor, Austin, TX 78758',
+      plusCode: 'C74H+2G Austin, Texas'
+    },
+
+    {
+      name: 'Proof',
+      homepage: 'https://useproof.com/',
+      careers: 'https://useproof.com/careers',
+      address: '200 E 6th St #310, Austin, TX 78701',
+      plusCode: '7795+4H Austin, Texas'
+    },
+
+    {
+      name: 'RapidDeploy',
+      homepage: 'https://www.rapiddeploy.com/',
+      careers: 'https://www.rapiddeploy.com/careers',
+      address: '119 Nueces St RapidDeploy, 2nd Floor, Austin, TX 78701',
+      plusCode: '7782+64 Austin, Texas'
+    },
+
+    {
+      name: 'RetailMeNot',
+      homepage: 'https://www.retailmenot.com/',
+      careers: 'https://jobs.lever.co/retailmenot',
+      address: '301 Congress Ave #700, Austin, TX 78701',
+      plusCode: '7784+5J Austin, Texas'
+    },
+
+    {
+      name: 'RunTitle',
+      homepage: 'https://www.runtitle.com/',
+      careers: 'https://www.runtitle.com/careers',
+      address: '210 Barton Springs Rd #225, Austin, TX 78704',
+      plusCode: '7762+2R Austin, Texas'
+    },
+
+    {
       name: 'ShipStation',
       homepage: 'https://www.shipstation.com/',
       careers: 'https://www.shipstation.com/careers/',
       address: '3800 N Lamar Blvd #220, Austin, TX 78756',
       plusCode: '8744+FR Austin, Texas'
+    },
+
+    {
+      name: 'Social Solutions',
+      homepage: 'https://www.socialsolutions.com/',
+      careers: 'https://www.socialsolutions.com/about-us/working-here/careers/',
+      address: '10801 N Mopac Expy, Austin, TX 78759',
+      plusCode: '97X9+6W Austin, Texas'
+    },
+
+    {
+      name: 'Square Root',
+      homepage: 'https://square-root.com/',
+      careers: 'https://square-root.com/careers/',
+      address: '508 Oakland Ave, Austin, TX 78703',
+      plusCode: '76FQ+FF Austin, Texas'
+    },
+
+    {
+      name: 'Stoplight Inc',
+      homepage: 'https://stoplight.io/',
+      careers: 'https://angel.co/stoplight/jobs/',
+      address: '1100 S 1st St, Austin, TX 78704',
+      plusCode: '763W+8J Austin, Texas'
     },
 
     {
@@ -99,11 +219,27 @@
     },
 
     {
+      name: 'thoughtbot',
+      homepage: 'https://thoughtbot.com/',
+      careers: 'https://thoughtbot.com/jobs#austin',
+      address: '1601 W 6th St, Austin, TX 78703',
+      plusCode: '76FP+VR Austin, Texas'
+    },
+
+    {
       name: 'uShip',
       homepage: 'https://www.uship.com/',
       careers: 'https://about.uship.com/careers/listings/',
       address: '205 E Riverside Dr, Austin, TX 78704',
       plusCode: '7734+W2 Austin, Texas'
+    },
+
+    {
+      name: 'YouEarnedIt',
+      homepage: 'https://youearnedit.com/',
+      careers: 'https://youearnedit.com/careers/',
+      address: '206 E 9th St, Austin, TX 78701',
+      plusCode: '77C6+64 Austin, Texas'
     }
   ]
   // THE LIST ENDS HERE
